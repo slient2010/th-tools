75dbe25c46331e4f1951921a908cf800347b2e97
Nelson.Young <slient2010@163.com>
2021-08-07 14:55:39 +0800

    format codes

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,38 @@
+use futures::executor::block_on;
 use serde::{Deserialize, Serialize};
 use serde_json::Result;
+use std::collections::HashMap;
 use std::fs;
 use tokio::time::{sleep, Duration};
-use std::collections::HashMap;
-use futures::executor::block_on;
 // use std::collections::HashMap;
 extern crate redis;
 use redis::Commands;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct searchDetails {
-  searchResults:Vec<CityInfo>,
+    searchResults: Vec<CityInfo>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct idValue {
-  Id: String,
-  Value: String,
+    Id: String,
+    Value: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 struct resStatus {
-   Timestamp: String,
-   Ack: String,
-   Errors: Vec<String>,
-   Extension: Vec<idValue>,
+    Timestamp: String,
+    Ack: String,
+    Errors: Vec<String>,
+    Extension: Vec<idValue>,
 }
-
 
 #[derive(Debug, Serialize, Deserialize)]
 struct ResponseResult {
-	Result: bool,
-  ErrorCode: i32,
-  Response: searchDetails,
-  ResponseStatus: resStatus,
+    Result: bool,
+    ErrorCode: i32,
+    Response: searchDetails,
+    ResponseStatus: resStatus,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -48,35 +47,35 @@ struct CityInfo {
     cityName: String,
     provinceId: i32,
     provinceName: String,
-    countryId:i32,
-    countryName:String,
-    lat:f32,
-    lon:f32,
-    gLat:f32,
-    gLon:f32,
-    gdLat:f32,
-    gdLon:f32,
-    bdLat:f32,
-    bdLon:f32,
-    domestic:bool,
-    mainland:bool,
-    distance:i32,
-    cityEName:String,
-    countryEName:String,
-    timeOffset:i32,
-    extendinfo:String,
-    issamecity:bool,
-    tokens:Vec<String>,
-    content:String,
-    source:String,
-    displayName:String,
-    displayType:String,
-    eName:String,
-    parentName:String,
+    countryId: i32,
+    countryName: String,
+    lat: f32,
+    lon: f32,
+    gLat: f32,
+    gLon: f32,
+    gdLat: f32,
+    gdLon: f32,
+    bdLat: f32,
+    bdLon: f32,
+    domestic: bool,
+    mainland: bool,
+    distance: i32,
+    cityEName: String,
+    countryEName: String,
+    timeOffset: i32,
+    extendinfo: String,
+    issamecity: bool,
+    tokens: Vec<String>,
+    content: String,
+    source: String,
+    displayName: String,
+    displayType: String,
+    eName: String,
+    parentName: String,
     matchType: i32,
     resultsType: i32,
     cStar: i32,
-    commentScore: f32
+    commentScore: f32,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -86,7 +85,7 @@ struct City {
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Province{
+struct Province {
     citys: Vec<City>,
     provinceName: String,
 }
@@ -96,13 +95,12 @@ struct Provinces {
     provinces: Vec<Province>,
 }
 
-
 #[derive(Serialize, Deserialize, Debug)]
 struct Req {
-  url: String,
-  status: u16,
-  headers: HashMap<String, String>,
-  body: Option<serde_json::Value>
+    url: String,
+    status: u16,
+    headers: HashMap<String, String>,
+    body: Option<serde_json::Value>,
 }
 
 #[tokio::main]
@@ -112,10 +110,10 @@ async fn main() {
     let p: Provinces = serde_json::from_str(&contents).expect("解析数据失败。");
     // 获取国内城市名称
     for province in p.provinces.iter() {
-        for pro in province.citys.iter(){
+        for pro in province.citys.iter() {
             let city_name = &pro.citysName;
             println!("{}", city_name);
-						block_on(get_city_id(city_name));
+            block_on(get_city_id(city_name));
             sleep(Duration::from_millis(100)).await;
         }
     }
@@ -125,9 +123,9 @@ async fn get_city_id(city: &String) -> i32 {
     let client = reqwest::Client::new();
     let url = "https://m.ctrip.com/restapi/soa2/21881/json/gaHotelSearchEngine";
     let city_name = &*city;
-		let city: &str = &city_name[..];  
+    let city: &str = &city_name[..];
     let mut data_raw = "{\"keyword\": \"".to_owned();
-		data_raw.push_str(city);
+    data_raw.push_str(city);
     let data_1: &str = "\",\"label\":\"\",\"searchType\":\"D\",\
         \"cityId\":0, \"district\":0,\"province\":0,\"webpSupport\":false,\
         \"platform\":\"online\", \
@@ -141,23 +139,20 @@ async fn get_city_id(city: &String) -> i32 {
         \"Union\":{\"AllianceID\":\"\",\"SID\":\"\",\"Ouid\":\"\"},\
         \"HotelExtension\":{\"group\":\"CTRIP\", \"hasAidInUrl\":false,\
         \"Qid\":\"304674774242\",\"WebpSupport\":false}}} ";
-		data_raw.push_str(data_1);
+    data_raw.push_str(data_1);
     println!("xxx{}", data_raw);
-		let res = client.post(url)
-				.body(data_raw)
-				.send()
-				.await.expect("error");
+    let res = client.post(url).body(data_raw).send().await.expect("error");
 
-		// let result_json_raw = format!("{}", res.text_with_charset("utf-8").await.expect("mmm"));
+    // let result_json_raw = format!("{}", res.text_with_charset("utf-8").await.expect("mmm"));
     // println!("{}", result_json_raw);
     let result_json = res.json::<ResponseResult>().await.expect("xxxyyy");
 
     // println!("{:#?}", res.json::<ResponseResult>().await.expect("xxxyyy"));
     let city_id = result_json.Response.searchResults[0].cityId;
-   
+
     let _ = save_to_redis(city_name, city_id);
-		sleep(Duration::from_millis(100)).await;
-		return 1
+    sleep(Duration::from_millis(100)).await;
+    return 1;
 }
 
 fn save_to_redis(city: &String, city_id: i32) -> redis::RedisResult<isize> {
@@ -165,6 +160,6 @@ fn save_to_redis(city: &String, city_id: i32) -> redis::RedisResult<isize> {
     let client = redis::Client::open("redis://:Vp8fICsV3P@hotel-spider-redis.teyixing.com/2")?;
     let mut con = client.get_connection()?;
     // throw away the result, just make sure it does not fail
-    let _ : () = con.set(city, city_id)?;
+    let _: () = con.set(city, city_id)?;
     con.get(city)
 }
